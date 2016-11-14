4be761707396286ea18b5d3bd9f54f99c7891d48
Morgan Quigley <morgan@osrfoundation.org>
2016-11-14 14:20:36 -0800

    don't instantiate a Rate object, and use printf instead of cout

--- rclcpp/minimal_talker/minimal_talker.cpp
+++ rclcpp/minimal_talker/minimal_talker.cpp
@@ -9,13 +9,12 @@ int main(int argc, char *argv[])
   auto publisher = node->create_publisher<std_msgs::msg::String>("chatter");
   auto message = std::make_shared<std_msgs::msg::String>();
   auto publish_count = 1;
-  rclcpp::WallRate rate(2);
   while (rclcpp::ok()) {
     message->data = "Hello, world! " + std::to_string(publish_count++);
-    std::cout << "Publishing: [" << message->data << "]" << std::endl;
+    printf("Publishing: [%s]\n", message->data.c_str());
     publisher->publish(message);
     rclcpp::spin_some(node);
-    rate.sleep();
+    rclcpp::utilities::sleep_for(std::chrono::milliseconds(500));
   }
   return 0;
 }
